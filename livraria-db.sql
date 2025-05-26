e5bfc81324e241bf3940fe491754c364edee7f0e
Maiara <50387178+maiaraguanha@users.noreply.github.com>
2025-05-26 20:40:53 -0300

    Adicionados o INSERT dos dados da tabela de produtos e de pedidos

--- livraria-db.sql
+++ livraria-db.sql
@@ -14,3 +14,13 @@ CREATE TABLE pedidos (
     data_pedido DATE,
     FOREIGN KEY (id_produto) REFERENCES produtos(id)
 );
+
+-- Dados da tabela de produtos
+INSERT INTO produtos (nome, preco, estoque) VALUES ('Harry Potter', 99.99, 10);
+INSERT INTO produtos (nome, preco, estoque) VALUES ('A Cabana', 50.00, 5);
+INSERT INTO produtos (nome, preco, estoque) VALUES ('Código da Vince', 45.00, 8);
+
+-- Dados da tabela de pedidos
+INSERT INTO pedidos (id_produto, quantidade, data_pedido) VALUES (1, 2, '2025-05-26');
+INSERT INTO pedidos (id_produto, quantidade, data_pedido) VALUES (2, 1, '2025-05-25');
+INSERT INTO pedidos (id_produto, quantidade, data_pedido) VALUES (3, 3, '2025-05-24');
